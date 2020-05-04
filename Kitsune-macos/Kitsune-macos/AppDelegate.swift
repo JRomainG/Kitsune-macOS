48b0cfbe846a0290f62b84415e604cf517ef2429
Jean-Romain Garnier <git@jean-romain.com>
2020-05-04 17:46:57 +0200

    Create Xcode project

---
+++ Kitsune-macos/Kitsune-macos/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Kitsune-macos
+//
+//  Created by Jean-Romain on 04/05/2020.
+//  Copyright © 2020 JustKodding. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+}

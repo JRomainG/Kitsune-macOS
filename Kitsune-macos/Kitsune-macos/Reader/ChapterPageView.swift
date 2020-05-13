01ea69281668c9a70c3df8c6c64aace59e9ece70
Jean-Romain Garnier <git@jean-romain.com>
2020-05-13 20:14:43 +0200

    Add loading indicator for chapter pages

--- Kitsune-macos/Kitsune-macos/Reader/ChapterPageView.swift
+++ Kitsune-macos/Kitsune-macos/Reader/ChapterPageView.swift
@@ -10,6 +10,8 @@ import Cocoa
 
 class ChapterPageView: NSImageView {
 
+    var loadingIndicator = NSProgressIndicator()
+
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
         commonInit()
@@ -29,13 +31,29 @@ class ChapterPageView: NSImageView {
         setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
         setContentCompressionResistancePriority(.defaultLow, for: .vertical)
         image = NSImage(named: "PagePlaceholder")
+
+        loadingIndicator.autoresizingMask = [.maxXMargin, .maxYMargin, .minXMargin, .minYMargin]
+        loadingIndicator.translatesAutoresizingMaskIntoConstraints = false
+        loadingIndicator.style = .spinning
+        loadingIndicator.isIndeterminate = true
+        loadingIndicator.controlSize = .regular
+        loadingIndicator.sizeToFit()
+        loadingIndicator.isDisplayedWhenStopped = false
+        loadingIndicator.stopAnimation(nil)
+        addSubview(loadingIndicator)
+        loadingIndicator.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        loadingIndicator.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
     }
 
     func setImage(with url: URL?) {
+        loadingIndicator.startAnimation(nil)
         sd_setImage(with: url,
                     placeholderImage: NSImage(named: "PagePlaceholder"),
-                    options: .decodeFirstFrameOnly,
-                    completed: nil)
+                    options: .decodeFirstFrameOnly) { (_, _, _, _) in
+                        DispatchQueue.main.async {
+                            self.loadingIndicator.stopAnimation(nil)
+                        }
+        }
     }
 
 }
